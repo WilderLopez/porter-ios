654ca67bf2d188de2e8166f09ad8c2b3c1e3743c
wilderlopez <wlopezelias@gmail.com>
2020-04-10 10:19:22 -0400

    Initial Commit

---
+++ QualityReaderQR/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QualityReaderQR
+//
+//  Created by Wilder Lopez on 4/10/20.
+//  Copyright © 2020 Wilder Lopez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
